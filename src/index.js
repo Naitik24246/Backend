7f3990b09d2c7b3b432997fe5238aba5e9c60797
Naitik Pal <naitikpal513@gmail.com>
2025-05-23 17:19:07 +0530

    Connected to MongoDB Atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-import mongoose from "mongoose";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+connectDB()
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app=express()
+// IIFE Function:- It is a function that runs immediately after it is defined.
+// Async Function:- For some tasks, like fetching data from a server, take time. 
+// We don’t want the program to freeze while waiting. That’s where asynchronous functions help.
+//  BASIC SYNTAX:[(()=>{})()]
+( async() => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR: ",error)
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on  the port ${process.env.PORT} `)
+        })
+    } catch (error){
+        console.error("ERROR: ",error)
+        throw error
+    }    
+})()
+*/
