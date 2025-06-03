2794927832ee1e9b0ea7f5507a58b180ff14a4d9
Naitik Pal <naitikpal513@gmail.com>
2025-06-03 19:43:46 +0530

    Add utilis files

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
 const app=express()
+// WHEN WE USE MIDDLE WARES AND FOR CONFIGURATION SETTINGS WE OFTEN USE APP.USE()
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit:"16kb"}))// when someone sends data in the form of json 
+
+app.use(express.urlencoded({extended:true,limit:"16kb"}))//To store data from the URL in the backend 
+
+app.use(express.static("public"))// To store pdf and images assests and show to to the browser
+
+app.use(cookieParser())
+
+
 export { app }
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,48 @@
+// A utils (short for utilities) file is where you store reusable helper functions that are not tied to any specific route or component — but are used across the project.
+
+
+//Promises wala 
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((error)=> next(error))
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// try catch wala 
+// const asyncHandler = (fn)=>async (req,res,next)=> {
+//     try {
+//         await fn(req,res,next)        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message: error.message
+//         })
+//     }
+
+// }
+
+
